0411b9eb5137cb6864ca959244aac40e6747db41
Aqib1422 <mraqibshafique@gmail.com>
2024-04-16 19:18:01 +0500

    namesec

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-let sidebar=document.querySelector('.sidebar')
-let sidebarbtn=document.querySelector('.sidebar-btn')
-let navclosebtn=document.querySelector('.close-btn-sidenav')
-sidebarbtn.addEventListener('click',()=>
-{
+let sidebar = document.querySelector('.sidebar')
+let sidebarbtn = document.querySelector('.sidebar-btn')
+let navclosebtn = document.querySelector('.close-btn-sidenav')
+sidebarbtn.addEventListener('click', () => {
     sidebar.classList.toggle('sidebar-animation')
 
 })
-navclosebtn.addEventListener('click',()=>
-{
-  
+navclosebtn.addEventListener('click', () => {
+
     sidebar.classList.remove('sidebar-animation')
 })
